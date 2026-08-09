fd901c8d505192aa1c8239432372a54ae5cb53cb
Sofia05090 <ana.ruedam@uniagustiniana.edu.co>
2026-08-09 19:27:21 +0000

    Cambios y corecciones pendientes 1

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -1,13 +1,12 @@
 //conexion con firebase y exporta los servicios que se esten usando en el proyecto
-<<<<<<< HEAD
 
-// Importa las funciones necesarias del SDK de Firebase
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Configuración de Firebase usando variables de entorno
-const firebaseConfig = {
+//las variables son leidas desde el archivo .env y se guarda en un objeto para inicializar la app de firebase
+const credenciales = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -17,35 +16,10 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Inicializa Firebase
-const app = initializeApp(firebaseConfig);
-
-// Exporta los servicios que vas a usar en el resto de la app
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
-export default app;
-=======
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-//las variables son leidas desde el archivo .env y se guarda en un objeto para inicializar la app de firebase
-const credenciales= {
-apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-appId: import.meta.env.VITE_FIREBASE_APP_ID,
-measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-};
-
 //se inicializa la app de firebase
 const app = initializeApp(credenciales);
 
 //exporta los servicios de firebase
-export const auth = getAuth(app); 
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
->>>>>>> a104040e108467b4702724d62516291f9e91a433
